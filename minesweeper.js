5dabf335088345ab0b5f51b1467010655457834a
chijason99 <chijason99@gmail.com>
2022-11-14 17:37:21 +0000

    finish the logic of the game

--- minesweeper.js
+++ minesweeper.js
@@ -1,5 +1,6 @@
 // the logic for the minesweeper game
-const TILE_STATUS = {
+
+export const TILE_STATUS = {
   HIDDEN: "hidden",
   MINE: "mine",
   MARKED: "marked",
@@ -8,9 +9,9 @@ const TILE_STATUS = {
 export function createBoard(boardSize, numberOfMines = 2) {
   const boardAry = [];
   const minesAry = setupMines(boardSize, numberOfMines);
-  for (let x = 1; x <= boardSize; x++) {
+  for (let y = boardSize; y >= 1; y--) {
     const row = [];
-    for (let y = 1; y <= boardSize; y++) {
+    for (let x = 1; x <= boardSize; x++) {
       const element = document.createElement("div");
       const tile = {
         element,
@@ -24,15 +25,8 @@ export function createBoard(boardSize, numberOfMines = 2) {
         },
       };
       tile.status = TILE_STATUS.HIDDEN;
-      if (minesAry.some(({ x, y }) => x === tile.x && y === tile.y)) {
-        tile.mine = true;
-      } else {
-        tile.mine = false;
-      }
+      checkMinePosition(minesAry, tile);
       //takeaway : can create a function to check for position, as ary.some would return true/false
-      tile.element.addEventListener("click", (e) => {
-        console.log(tile.x, tile.y);
-      });
       // adding the marked function for the right click
 
       row.push(tile);
@@ -48,13 +42,13 @@ function setupMines(boardSize, numberOfMines) {
   const mines = [];
   // setting up x,y location for the mine
   // the mines cannot have the same location
-
   while (mines.length < numberOfMines) {
     const mine = {
       x: randomCoordinate(boardSize),
       y: randomCoordinate(boardSize),
     };
-    if (mines.every(({ x, y }) => x !== mine.x && y !== mine.y)) {
+    if (mines.every(({ x, y }) => x !== mine.x || y !== mine.y)) {
+      //key takeaway: if using &&, it means that every x and u should be unique, which only gives at most 9 coordinates
       mines.push(mine);
     }
   }
@@ -64,6 +58,13 @@ function setupMines(boardSize, numberOfMines) {
 function randomCoordinate(boardSize) {
   if (boardSize < 2) return;
   return Math.ceil(Math.random() * (boardSize - 1));
+}
+function checkMinePosition(mineAry, tile) {
+  if (mineAry.some(({ x, y }) => x === tile.x && y === tile.y)) {
+    tile.mine = true;
+  } else {
+    tile.mine = false;
+  }
 }
 
 export function markTile(tile) {
@@ -77,4 +78,66 @@ export function markTile(tile) {
     default:
       return;
   }
+}
+
+export function revealTile(board, tile) {
+  // case : if the tile is a mine
+  if (tile.status !== TILE_STATUS.HIDDEN) return;
+  if (tile.mine) {
+    // show the mine, end the game
+    tile.status = TILE_STATUS.MINE;
+    return;
+  }
+  // show how many mines are inside the 8 sqrs nearby
+  const tilesAround = getTilesAround(tile, board).filter((t) => t != undefined);
+  tile.status = TILE_STATUS.NUMBER;
+  const minesNearby = tilesAround.filter((t) => t.mine === true).length;
+  if(minesNearby === 0){
+    tilesAround.forEach(revealTile.bind(null,board))
+    // key takeaway: using the recursive function to do repeat work, using bind to auto bind the parameters
+  }else{
+    tile.element.innerText = minesNearby;
+  }
+}
+function getTilesAround(tile, board) {
+  const tilesAround = [];
+  const { x, y } = tile;
+  const topTile = getSingleTile(board, x, y + 1);
+  const bottomTile = getSingleTile(board, x, y - 1);
+  const leftTile = getSingleTile(board, x - 1, y);
+  const rightTile = getSingleTile(board, x + 1, y);
+  const topLeftTile = getSingleTile(board, x - 1, y + 1);
+  const topRightTile = getSingleTile(board, x + 1, y + 1);
+  const bottomLeftTile = getSingleTile(board, x - 1, y - 1);
+  const bottomRightTile = getSingleTile(board, x + 1, y - 1);
+  tilesAround.push(
+    topTile,
+    bottomTile,
+    leftTile,
+    rightTile,
+    topLeftTile,
+    topRightTile,
+    bottomLeftTile,
+    bottomRightTile
+  );
+  return tilesAround;
+}
+
+function getSingleTile(board, x, y) {
+  return board[10 - y]?.find((t) => t.x === x);
+  // using the option chaining
+}
+
+export function checkWin(board){
+  return board.every(row => {
+    return row.every(tile => {
+      return (tile.status === TILE_STATUS.NUMBER) || (tile.mine && ((tile.status === TILE_STATUS.HIDDEN) || (tile.status === TILE_STATUS.MARKED) ) ) 
+    })
+  })
+}
+
+export function checkLose(board){
+  return board.some(row => {
+    return row.some(tile => tile.status === TILE_STATUS.MINE)
+  })
 }
--- script.js
+++ script.js
@@ -1,26 +1,78 @@
 // the ui/design of the game
-import { createBoard, markTile } from "./minesweeper.js";
+import {
+  checkLose,
+  checkWin,
+  createBoard,
+  markTile,
+  revealTile,
+  TILE_STATUS,
+} from "./minesweeper.js";
 // 1. generate the board
 // key takeaway: creating functions that we can dynamically edit the input and output, instead of hardcoding the layout
 // console.log(board)
 const boardElement = document.querySelector(".board");
-const BOARD_SIZE = 10
-const NUM_OF_MINES = 10
+const mineCountText = document.querySelector("[data-mine-count]");
+const subText = document.querySelector('.subtext')
+const BOARD_SIZE = 10;
+const NUM_OF_MINES = 3;
+mineCountText.innerText = NUM_OF_MINES;
 const board = createBoard(BOARD_SIZE, NUM_OF_MINES);
-// for(const row of board){
-//     for(const tile of row){
-//         boardElement.appendChild(tile.element)
-//         tile.element.addEventListener("contextmenu", (e) => {
-//             e.preventDefault();
-//             markTile(tile)
-//           });
-//         tile.element.addEventListener('click', e => {
-//             console.log(tile.x, tile.y)
-//         })
-//     }
-// } 
-boardElement.style.setProperty("--size", BOARD_SIZE);
+for (const row of board) {
+  for (const tile of row) {
+    boardElement.appendChild(tile.element);
+    // 3. right click
+    tile.element.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      markTile(tile);
+      //minus the number of marked tiles from the mine-count
+      mineCountText.innerText = NUM_OF_MINES - getMarkedTileNum();
+    });
+    // 2. left click
+    //reveal the tiles
+    tile.element.addEventListener("click", (e) => {
+      revealTile(board, tile);
+      checkGameOver(board);
+    });
+  }
+}
 //key takeaway: can use variable in css, then use js to set it up
-// 2. left click
-// 3. right click
-// 4. game over logic
+boardElement.style.setProperty("--size", BOARD_SIZE);
+
+function getMarkedTileNum() {
+  const markedTileNum = board.reduce((acc, row) => {
+    return (
+      row.filter((tile) => tile.status === TILE_STATUS.MARKED).length + acc
+    );
+  }, 0);
+  // key takeaway: the usage of reduce method on counting total num
+  return markedTileNum;
+}
+
+function checkGameOver(board) {
+  const win = checkWin(board);
+  const lose = checkLose(board);
+  console.log(lose)
+  if(win || lose){
+    boardElement.addEventListener('click', stopProp, {capture:true} )
+    boardElement.addEventListener('contextmenu', stopProp, {capture:true} )
+  }
+  if (win) {
+    subText.innerText = 'You Win!'
+  } 
+  if (lose) {
+    subText.innerText = 'You Lose.'
+    for(const row of board){
+        for(const tile of row){
+            if(tile.status === TILE_STATUS.MARKED) markTile(tile)
+            if(tile.mine) tile.status = TILE_STATUS.MINE
+        }
+    }
+
+  }
+
+}
+
+function stopProp(e){
+    e.stopPropagation();
+};
+
